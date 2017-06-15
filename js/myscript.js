ce5e2da564862a5e905768b4710ca06a4c2ece8f
micos7 <micos7@gmail.com>
2017-06-15 11:56:07 +0300

    Scrollspy styling

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,35 @@
+$(function(){
+"use strict";
+//Activate Scrollspy
+var topoffset = 50; //menu height
+
+$('body').scrollspy({
+  target: 'header .navbar',
+  offset: topoffset
+})
+
+var hash = $(this).find('li.active a').attr('href');
+
+  if(hash !== '#featured'){
+    $('header nav').addClass('inbody');
+  } else {
+    $('header nav').removeClass('inbody');
+  }
+
+//inbody class when scrollspy fires
+$('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
+  var hash = $(this).find('li.active a').attr('href');
+
+  if(hash !== '#featured'){
+    $('header nav').addClass('inbody');
+  } else {
+    $('header nav').removeClass('inbody');
+  }
+});
+
+
 $('.carousel').carousel({
   interval: false
+});
+
 });
