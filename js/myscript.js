b800ba377ff3d5cf2af2f3408b23f06ce94d6a8b
micos7 <micos7@gmail.com>
2017-06-15 12:05:22 +0300

    Smooth scrolling

--- js/myscript.js
+++ js/myscript.js
@@ -8,6 +8,8 @@ $('body').scrollspy({
   offset: topoffset
 })
 
+
+//add inbody class
 var hash = $(this).find('li.active a').attr('href');
 
   if(hash !== '#featured'){
@@ -26,6 +28,22 @@ $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
     $('header nav').removeClass('inbody');
   }
 });
+
+ //Use smooth scrolling when clicking on navigation
+  $('.navbar a[href*=\\#]:not([href=\\#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === 
+      this.pathname.replace(/^\//,'') && 
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset+2
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrollin
 
 
 $('.carousel').carousel({
